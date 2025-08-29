fd090f1680afd2b11bf1321c6ec80ac371c9b1f6
Ali Shah Rasouli <alishahrasouli98@yahoo.com>
2025-08-29 16:49:17 +0430

    middleware and route for users and product added

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,20 @@
 <?php
-
+namespace App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RoleController;
+use Illuminate\Support\Facades\UserController;
+use Illuminate\Support\Facades\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function (){ 
+    if (Auth::check()) { 
+        return redirect()->route('home');
+    } return redirect()->route('login'); });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth']) ->group(function(){ Route::resource('roles', RoleController::class);
+Route::resource('users', UserController::class);
+Route::resource('products', ProductController::class); }); 
